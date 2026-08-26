c4efe1bf1fde1899eb159eb9fa44bf1b6b041a07
Madhaw Kumar Bagadia <madhawbagadiajss@gmail.com>
2026-08-26 19:39:14 +0530

    Configure production server URL

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerURL = "";
+export const ServerURL = import.meta.env.VITE_SERVER_URL;
 
 function App() {
 
